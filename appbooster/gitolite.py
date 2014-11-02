573bf0200f7c074f64f40d951f7507289c705443
Kaiwen Xu <kevin@kevxu.net>
2014-11-01 22:22:10 -0400

    Add add_user and add_repo functions to gitolite.

--- appbooster/gitolite.py
+++ appbooster/gitolite.py
@@ -11,6 +11,9 @@ GITOLITE_CONF_DEFAULT = """repo gitolite-admin
 
 include "*.conf"
 """
+GITOLITE_CONF_REPO_TEMPLATE = """repo %s
+%s
+"""
 
 
 def init():
@@ -21,15 +24,46 @@ def init():
     if not os.path.exists(GITOLITE_CONF_FILE):
         raise RuntimeError("Cannot find gitolite.conf")
 
-    with open(GITOLITE_CONF_FILE, 'rw') as gitolite_conf:
+    with open(GITOLITE_CONF_FILE, 'w+') as gitolite_conf:
         gitolite_conf_content = gitolite_conf.read()
 
         if gitolite_conf_content != GITOLITE_CONF_DEFAULT:
             gitolite_conf.write(GITOLITE_CONF_DEFAULT)
 
 
-def addusr(user, public_key):
-    pass
+def add_usr(user, public_key):
+    # TODO: define user object model
+
+    user_key_filename = user + '.pub'
+    user_key_path = os.path.join(KEY_DIR, user_key_filename)
+
+    with open(user_key_path, 'wb') as user_key_file:
+        user_key_file.write(public_key)
+
+
+def add_repo(repo, users):
+    # TODO: define repo object model
+    # TODO: define user object model
+
+    repo_name = repo
+
+    repo_conf_filename = repo_name + '.conf'
+    repo_conf_path = os.path.join(CONF_DIR, repo_conf_filename)
+
+    if isinstance(users, str):
+        repo_users = [users]
+    else:
+        repo_users = users
+
+    repo_users_str_buf = []
+    for repo_user in repo_users:
+        repo_users_str_buf.append('    RW      =   %s' % repo_user)
+    repo_users_str = ''.join(repo_users_str_buf)
+
+    repo_conf = GITOLITE_CONF_REPO_TEMPLATE % (repo_name, repo_users_str)
+
+    with open(repo_conf_path, 'wb') as repo_conf_file:
+        repo_conf_file.write(repo_conf)
 
 
 if __name__ == '__main__':
